f8056983c5b29fb89ec0755a75e45696b14b13f2
Phamandy00 <Phamandy00@yahoo.com>
2020-02-07 13:21:48 -0500

    added comments

--- src/Main.java
+++ src/Main.java
@@ -17,36 +17,47 @@ public class Main
         linkStart();
     }
 
-    private static void linkStart()
+    private static void linkStart() //Start of the entire game
     {
         System.out.println(newWord);
         System.out.println("Welcome to the game of Hangman. You have 6 lives and you have to guess the word correctly");
-        while(isPlaying) {
-            if (newGame == true) {
+        while(isPlaying)
+        {
+            if (newGame == true)
+            {
                 newWord = getRandomWord(words);
                 System.out.println(newWord);
             }
             System.out.println("Enter a guess");
             char guess = getGuess();
-            if (!Character.isLetter(guess)) {
+            if (!Character.isLetter(guess))
+            {
                 System.out.println("Please enter a valid letter");
                 newGame = false;
             }
-            if (isAlreadyGuessed(guess)) {
+            if (isAlreadyGuessed(guess))
+            {
                 System.out.println("Please enter a different letter");
-            } else {
+            }
+            else
+            {
                 usedLetters.add(guess);
                 guessedWord = missingLetters(newWord, usedLetters);
                 System.out.println(guessedWord);
                 newGame = false;
-                if ()
+                if(gameOver())
                 {
 
                 }
             }
         }
     }
-    private static String missingLetters(String word, ArrayList<Character> letters)
+
+    private static void gameOver()
+    {
+        
+    }
+    private static String missingLetters(String word, ArrayList<Character> letters) //Returns the word with the correct guessed letters
     {
         String filledInWord = "";
         for(int i = 0; i < word.length(); i++)
@@ -66,21 +77,21 @@ public class Main
         return filledInWord;
     }
 
-    private static String getRandomWord(String[] words)
+    private static String getRandomWord(String[] words) //Takes a random word from the array list and returns one of the words
     {
         Random rand = new Random();
         int index = rand.nextInt(words.length);
         return words[index];
     }
 
-    private static char getGuess()
+    private static char getGuess() //Returns the letter guess from the user
     {
         Scanner scan = new Scanner(System.in);
         char guess = scan.nextLine().toLowerCase().charAt(0);
         return guess;
     }
 
-    private static boolean isAlreadyGuessed(char guess)
+    private static boolean isAlreadyGuessed(char guess) //Checks if the letter has already been guessed in the array list
     {
         for(int i = 0; i < usedLetters.size(); i++)
         {
